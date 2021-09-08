aeffdd798a0bdd61abbaccd7573d7bb1879b6b26
Việt Dương <duongquocv@gmail.com>
2021-09-08 16:59:02 +0700

    done

---
+++ Client/database/migrations/2021_09_08_072333_create_product_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prodcut', function (Blueprint $table) {
+            $table->id();
+
+            $table->integer('productId')->autoIncrement();
+            $table->string('name');
+            $table->integer('price');
+            $table->string('avatar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prodcut');
+    }
+}
